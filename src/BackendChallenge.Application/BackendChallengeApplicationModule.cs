e6b3ac523a8f1dc4c5028d6ab46c585ad89b2bb8
Engin Can Altınöz <altinozengincan@gmail.com>
2024-07-11 02:13:38 +0300

    UPDATE: worker added to module

--- src/BackendChallenge.Application/BackendChallengeApplicationModule.cs
+++ src/BackendChallenge.Application/BackendChallengeApplicationModule.cs
@@ -1,30 +1,36 @@
-﻿using Abp.AutoMapper;
+﻿using System.Reflection;
+using Abp.AutoMapper;
 using Abp.Modules;
 using Abp.Reflection.Extensions;
+using Abp.Threading.BackgroundWorkers;
 using BackendChallenge.Authorization;
+using BackendChallenge.Movies.Jobs;
 
-namespace BackendChallenge
+namespace BackendChallenge;
+
+[DependsOn(
+  typeof(BackendChallengeCoreModule),
+  typeof(AbpAutoMapperModule))]
+public class BackendChallengeApplicationModule : AbpModule
 {
-    [DependsOn(
-        typeof(BackendChallengeCoreModule), 
-        typeof(AbpAutoMapperModule))]
-    public class BackendChallengeApplicationModule : AbpModule
-    {
-        public override void PreInitialize()
-        {
-            Configuration.Authorization.Providers.Add<BackendChallengeAuthorizationProvider>();
-        }
+  public override void PreInitialize()
+  {
+    Configuration.Authorization.Providers.Add<BackendChallengeAuthorizationProvider>();
 
-        public override void Initialize()
-        {
-            var thisAssembly = typeof(BackendChallengeApplicationModule).GetAssembly();
+    IBackgroundWorkerManager workerManager = IocManager.Resolve<IBackgroundWorkerManager>();
+    workerManager.Add(IocManager.Resolve<UpdateMovieDbWorker>());
+  }
 
-            IocManager.RegisterAssemblyByConvention(thisAssembly);
+  public override void Initialize()
+  {
+    Assembly thisAssembly = typeof(BackendChallengeApplicationModule).GetAssembly();
 
-            Configuration.Modules.AbpAutoMapper().Configurators.Add(
-                // Scan the assembly for classes which inherit from AutoMapper.Profile
-                cfg => cfg.AddMaps(thisAssembly)
-            );
-        }
-    }
+    IocManager.RegisterAssemblyByConvention(thisAssembly);
+
+    Configuration.Modules.AbpAutoMapper()
+      .Configurators.Add(
+        // Scan the assembly for classes which inherit from AutoMapper.Profile
+        cfg => cfg.AddMaps(thisAssembly)
+      );
+  }
 }
